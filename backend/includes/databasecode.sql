feabc07a8dab1c828d7becfc0181b029d1cd51f5
Ruchir Mehta <ruchirmehta14@gmail.com>
2018-11-25 13:15:23 -0800

    Delete databasecode.sql

--- backend/includes/databasecode.sql
+++
@@ -1,8 +0,0 @@
--- CREATE TABLE users (
---     idUsers varchar(256) not null,
---     uidUsers varchar(256) not null,
---     emailUsers varchar(256) not null,
---     pwdUsers varchar(256) not null
--- );
-
--- INSERT INTO users (idUsers, uidUsers, emailUsers, pwdUsers) VALUES ();
