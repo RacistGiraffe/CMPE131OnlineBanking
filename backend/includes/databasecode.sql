d64e62058321090b8cd922d0580f2369814989c6
Ruchir Mehta <ruchirmehta14@gmail.com>
2018-11-28 01:21:06 -0800

    Delete databasecode.sql

--- backend/includes/databasecode.sql
+++
@@ -1,9 +0,0 @@
--- CREATE TABLE users (
---     idUsers int(11) AUTO_INCREMENT PRIMARY KEY not null,
---     uidUsers varchar(256) not null,
---     emailUsers varchar(256) not null,
---     pwdUsers varchar(256) not null,
---     balanceUsers decimal(10,2) not null
--- );
-
--- INSERT INTO users (idUsers, uidUsers, emailUsers, pwdUsers, balanceUsers) VALUES (?,?,?,?,?);
